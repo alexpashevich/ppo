gifs writing fixed, a critical bug with stacking the frames fixed, a critical bug with the normalization is fixed, stats logging is fixed

ppo/parts/mime_env.py
@@ -138,8 +138,8 @@ class MiMEEnv(object):
                 # 1) transforms.ToPILImage expects 3D tensor, so we use [None]
                 # 2) transforms.ToPILImage expects image between 0. and 1.
                 # 3) we remove the extra dim with [0] afterwards
-                obs_channel_float = obs_channel[..., None].astype('float32') / 255
-                observation.append(self.image_transform(obs_channel_float)[0].numpy())
+                obs_transformed = self.image_transform(obs_channel[..., None])[0].numpy()
+                observation.append(obs_transformed)
             observation = np.stack(observation)
         return observation
 

ppo/tools/envs.py
@@ -3,6 +3,8 @@ import gym
 import numpy as np
 import torch
 
+from collections import deque
+
 from baselines import bench
 from baselines.common.atari_wrappers import make_atari, wrap_deepmind
 from baselines.common.vec_env import VecEnvWrapper
@@ -153,32 +155,50 @@ class VecPyTorchFrameStack(VecEnvWrapper):
         high = np.repeat(wos.high, self.nstack, axis=0)
 
         if device is None:
-            device = torch.device('cpu')
-        self.stacked_obs = torch.zeros((venv.num_envs,) + low.shape).to(device)
+            self.device = torch.device('cpu')
+        else:
+            self.device = device
+        # self.stacked_obs = torch.zeros((venv.num_envs,) + low.shape).to(device)
+        self.stacked_obs = [deque(maxlen=nstack) for _ in range(venv.num_envs)]
 
         observation_space = gym.spaces.Box(
             low=low, high=high, dtype=venv.observation_space.dtype)
         VecEnvWrapper.__init__(self, venv, observation_space=observation_space)
 
+    def _deque_to_tensor(self):
+        obs_list = []
+        for d in self.stacked_obs:
+            obs_list.append(torch.cat(tuple(d)))
+        tensor = torch.stack(obs_list)
+        return tensor.to(self.device)
+
     def step_wait(self):
         obs, rews, news, infos = self.venv.step_wait()
-        self.stacked_obs[:, :-self.shape_dim0] = \
-            self.stacked_obs[:, self.shape_dim0:]
+        # no need to do this with the deque
+        # self.stacked_obs[:, :-self.shape_dim0] = \
+        #     self.stacked_obs[:, self.shape_dim0:]
         for (i, new) in enumerate(news):
+            self.stacked_obs[i].append(obs[i])
             if new:
-                # we do not want zeros, we want the first observation to be tiled
-                # self.stacked_obs[i] = 0
-                self.stacked_obs[i] = obs[i].repeat(self.nstack, 1, 1)
-        self.stacked_obs[:, -self.shape_dim0:] = obs
-        return self.stacked_obs, rews, news, infos
+                for _ in range(self.nstack - 1):
+                    self.stacked_obs[i].append(obs[i])
+            # if new:
+            #     # we do not want zeros, we want the first observation to be tiled
+            #     # self.stacked_obs[i] = 0
+            #     self.stacked_obs[i] = obs[i].repeat(self.nstack, 1, 1)
+        # self.stacked_obs[:, -self.shape_dim0:] = obs
+        return self._deque_to_tensor(), rews, news, infos
 
     def reset(self):
         obs = self.venv.reset()
-        self.stacked_obs.zero_()
+        # self.stacked_obs.zero_()
         # we do not want zeros, we want the first observation to be tiled
         # self.stacked_obs[:, -self.shape_dim0:] = obs
-        self.stacked_obs = obs.repeat(1, self.nstack, 1, 1)
-        return self.stacked_obs
+        for i in range(len(obs)):
+            for _ in range(self.nstack):
+                self.stacked_obs[i].append(obs[i])
+        # self.stacked_obs = obs.repeat(1, self.nstack, 1, 1)
+        return self._deque_to_tensor()
 
     def close(self):
         self.venv.close()

ppo/tools/utils.py
@@ -8,9 +8,10 @@ import socket
 import numpy as np
 
 from ppo.tools.envs import VecNormalize, make_vec_envs
+import ppo.tools.stats as stats
+import ppo.tools.gifs as gifs
 
 from PIL import Image
-from bc.utils import videos
 
 # Get a render function
 def get_render_func(venv):
@@ -75,18 +76,7 @@ def try_to_load_model(logdir):
             loaded = True
             break
         except Exception as e:
-            # print('did not load a policy from {}'.format(os.path.join(logdir, model_name)))
             pass
-    if not loaded:
-        files = glob.glob(os.path.join(logdir, '*.monitor.csv'))
-        for f in files:
-            os.remove(f)
-        try:
-            os.makedirs(os.path.join(logdir, '_eval'))
-        except OSError:
-            files = glob.glob(os.path.join(logdir, '_eval', '*.monitor.csv'))
-            for f in files:
-                os.remove(f)
     return loaded, loaded_tuple
 
 def load_from_checkpoint(policy, path, device):
@@ -130,101 +120,74 @@ def evaluate(policy, args_train, device, envs, eval_envs, env_render=None):
         vec_norm.eval()
         vec_norm.ob_rms = get_vec_normalize(envs).ob_rms
 
-    returns_eval, lengths_eval = [], []
     obs = eval_envs.reset()
     if env_render:
         env_render.reset()
     eval_recurrent_hidden_states = torch.zeros(
         args.num_processes, policy.recurrent_hidden_state_size, device=device)
     eval_masks = torch.zeros(args.num_processes, 1, device=device)
-    returns_current = np.array([0] * args.num_processes, dtype=np.float)
-    lengths_current = np.array([0] * args.num_processes, dtype=np.float)
+    stats_global, stats_local = stats.init(args, eval=True)
 
-    frames = [[] for _ in range(args.num_processes)]
-    skill_actions = [[] for _ in range(args.num_processes)]
-    master_actions = [[] for _ in range(args.num_processes)]
-    idx_video = 0
-    gifs_dir = os.path.join(args.logdir, args.timestamp, 'gifs')
-    if not os.path.exists(gifs_dir):
-        os.mkdir(gifs_dir)
+    if args.save_gifs:
+        gifs_global, gifs_local = gifs.init(args.num_processes)
+    else:
+        gifs_global = None
 
     print('Evaluating...')
-    while len(returns_eval) < args.num_eval_episodes:
+    while len(stats_global['return']) < args.num_eval_episodes:
         with torch.no_grad():
             _, action, _, eval_recurrent_hidden_states = policy.act(
                 obs, eval_recurrent_hidden_states, eval_masks, deterministic=True)
 
         # Observe reward and next obs
-        # obs, reward, done, infos = eval_envs.step(action)
         if not args.use_bcrl_setup:
             obs, reward, done, infos = eval_envs.step(action)
             if env_render is not None:
                 env_render.step(action[:1].numpy())
         else:
-            stack_obs, stack_act, reward, done, infos = do_master_step(
-                action, obs, args.timescale, policy, eval_envs, env_render)
-            obs = stack_obs[-1]
+            master_step_output = do_master_step(
+                action, obs, args.timescale, policy, eval_envs, env_render,
+                return_observations=args.save_gifs)
+            obs, reward, done, infos = master_step_output[:4]
+            if args.save_gifs:
+                # saving gifs only works for the BCRL setup
+                gifs_global, gifs_local = gifs.update(
+                    gifs_global, gifs_local, action, done, *master_step_output[4:])
 
-        for obs_skill, act_skill in zip(stack_obs, stack_act):
-            for i, (obs_worker, act_worker) in enumerate(zip(obs_skill, act_skill)):
-                frame = np.array((0.5+obs_worker.cpu().numpy()*0.5)*255, dtype=np.uint8)
-                frames[i].append(frame)
-                skill_actions[i].append(act_worker)
-        for i, action_worker in enumerate(action.cpu().numpy()):
-            master_actions[i].append(action_worker[0])
-
-        idxs_done = np.where(done)[0]
-        for idx in idxs_done:
-            print('Writing video {}'.format(idx_video))
-            print('Master actions {}'.format(master_actions[idx]))
-            gif_name = '{:02}.gif'.format(idx_video)
-            videos.write_video(frames[idx], os.path.join(gifs_dir, gif_name))
-            np.savez(os.path.join(gifs_dir, '{:02}.npz'.format(idx_video)), actions=skill_actions[idx])
-            idx_video += 1
-            frames[idx] = []
-            master_actions[idx] = []
-            skill_actions[idx] = []
-
-        returns_current, lengths_current = returns_current + reward[:, 0].numpy(), lengths_current + 1
-        # append returns of envs that are done (reset)
-        returns_eval.extend(returns_current[np.where(done)])
-        lengths_eval.extend(lengths_current[np.where(done)])
-        # zero out returns of the envs that are done (reset)
-        returns_current[np.where(done)], lengths_current[np.where(done)] = 0, 0
+        stats_global, stats_local = stats.update(stats_global, stats_local, reward, done, infos, args)
         eval_masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
-
-    return eval_envs, returns_eval, lengths_eval
+    return eval_envs, stats_global, gifs_global
 
 
 def do_master_step(
-        master_action, master_obs, master_timescale, policy, envs, env_render=None):
-    # TODO: do we want to print it? (all the master actions)
-    print_master_action = False
+        master_action, master_obs, master_timescale, policy, envs,
+        env_render=None, print_master_action=True, return_observations=False):
     if print_master_action:
         if hasattr(master_action, 'cpu'):
             master_action = master_action.cpu().numpy()[:, 0]
         print('master action = {}'.format(master_action))
     master_reward = 0
     worker_obs = master_obs
-    stack_obs = [master_obs]
-    stack_act = []
+    if return_observations:
+        stack_obs, stack_act = [master_obs], []
     master_done = np.array([False] * master_action.shape[0])
     for _ in range(master_timescale):
         with torch.no_grad():
             worker_action = policy.get_worker_action(master_action, worker_obs)
         worker_obs, reward, done, infos = envs.step(worker_action)
-        stack_obs.append(worker_obs)
-        stack_act.append(worker_action.cpu().numpy())
         if env_render is not None:
             env_render.step(worker_action[:1].numpy())
+        if return_observations:
+            stack_obs.append(worker_obs)
+            stack_act.append(worker_action.cpu().numpy())
         master_reward += reward
         master_done = np.logical_or(master_done, done)
-        if done.any() and not done.all():
-            # print('WARNING: one or several envs are done but not all during the macro step!')
-            pass
         if master_done.all():
             break
-    return stack_obs, stack_act, master_reward, master_done, infos
+    if not return_observations:
+        return worker_obs, master_reward, master_done, infos
+    else:
+        return worker_obs, master_reward, master_done, infos, stack_obs, stack_act
 
 
 def get_device(device):