(critical) rgbd normalization fix, eval fix, gifs try catch added

ppo/parts/mime_env.py
@@ -128,26 +128,40 @@ class MiMEEnv(object):
                     observation = np.concatenate((observation, obs_value))
         else:
             if not self.use_bcrl_setup:
-                if isinstance(observs, dict):
-                    obs_big = self._extract_obs(observs)
-                    obs_big = np.tile(obs_big, 3).reshape((240, 320, -1))
-                    # obs_big: 240 x 320 x 3(12)
-                    obs_big = np.moveaxis(obs_big, 2, 0)
-                    # obs_big: 3(12) x 240 x 320
-                else:
-                    obs_big = np.concatenate([
-                        self._extract_obs(obs) for obs in observs[-self.num_frames:]])
-                    # obs_big: 3(12) x 240 x 320
-            else:
-                obs_big = self._extract_obs(observs)
+                raise NotImplementedError('implement the proper normalization')
+            #     if isinstance(observs, dict):
+            #         obs_big = self._extract_obs(observs)
+            #         obs_big = np.tile(obs_big, 3).reshape((240, 320, -1))
+            #         # obs_big: 240 x 320 x 3(12)
+            #         obs_big = np.moveaxis(obs_big, 2, 0)
+            #         # obs_big: 3(12) x 240 x 320
+            #     else:
+            #         obs_big = np.concatenate([
+            #             self._extract_obs(obs) for obs in observs[-self.num_frames:]])
+            #         # obs_big: 3(12) x 240 x 320
+            # else:
+            #     obs_big = self._extract_obs(observs)
+            # for obs_channel in obs_big:
+            #     # 1) transforms.ToPILImage expects 3D tensor, so we use [None]
+            #     # 2) transforms.ToPILImage expects image between 0. and 1.
+            #     # 3) we remove the extra dim with [0] afterwards
+            #     obs_transformed = self.image_transform(obs_channel[..., None])[0].numpy()
+            #     observation.append(obs_transformed)
+            if self.observation_type == 'depth':
+                obs_keys = ('depth0',)
+            elif self.observation_type == 'rgbd':
+                obs_keys = ('rgb0', 'depth0')
             observation = []
-            for obs_channel in obs_big:
-                # 1) transforms.ToPILImage expects 3D tensor, so we use [None]
-                # 2) transforms.ToPILImage expects image between 0. and 1.
-                # 3) we remove the extra dim with [0] afterwards
-                obs_transformed = self.image_transform(obs_channel[..., None])[0].numpy()
+            for obs_key in obs_keys:
+                # this might be the depth channel or the rgb whole
+                obs_big = observs[obs_key]
+                if len(obs_big.shape) == 2:
+                    # transforms.ToPILImage expects 3D tensor, so we use [None]
+                    obs_big = obs_big[..., None]
+                # we remove the extra dim with [0] afterwards
+                obs_transformed = self.image_transform(obs_big).numpy()
                 observation.append(obs_transformed)
-            observation = np.stack(observation)
+            observation = np.concatenate(observation, axis=0)
         return observation
 
     def _get_null_action_dict(self):

ppo/scripts/eval.py
@@ -26,11 +26,17 @@ def get_args():
                         help='episodes max length')
     # parser.add_argument('--no-render', action='store_true', default=False,
     #                     help='whether to render the environment')
+    parser.add_argument('--logdir', type=str, default=None, help='not used')
+    parser.add_argument('--timestamp', type=str, default=None, help='not used')
     args = parser.parse_args()
     return args
 
-def run_evaluation(load_path, args, device, epoch):
-    policy, _, ob_rms, step, args_old = torch.load(load_path)
+def run_evaluation(load_path, args, device, epoch, eval_envs):
+    if str(device) == 'cpu':
+        load_tuple = torch.load(load_path, map_location=lambda storage, loc: storage)
+    else:
+        load_tuple = torch.load(load_path)
+    policy, _, ob_rms, step, args_old = load_tuple
     assert args_old.eval_offline, 'evaluation was already done'
     args_old.eval_max_length_factor = 1
     args_old.max_length = args.max_length
@@ -46,14 +52,14 @@ def run_evaluation(load_path, args, device, epoch):
     env_render = None
     # env_render_train, env_render_eval = create_render_env(args, device) if render else (None, None)
 
-    _, stats, gifs = evaluate(policy=policy,
-                              args_train=args_old,
-                              device=device,
-                              train_envs_or_ob_rms=ob_rms,
-                              eval_envs=None,
-                              env_render=env_render)
+    eval_envs, stats, gifs = evaluate(policy=policy,
+                                      args_train=args_old,
+                                      device=device,
+                                      train_envs_or_ob_rms=ob_rms,
+                                      eval_envs=eval_envs,
+                                      env_render=env_render)
     num_env_steps = (epoch + 1) * args_old.num_processes * args_old.num_frames_per_update
-    return stats, gifs, num_env_steps
+    return stats, gifs, num_env_steps, eval_envs
 
 def main():
     args = get_args()
@@ -61,26 +67,24 @@ def main():
     seed_torch(args)
     print('Evaluating the experiments on {}'.format(device))
 
-    assert not os.path.exists(os.path.join(args.expdir, 'gifs')), 'gifs dir already exist, seems'
+    #assert not os.path.exists(os.path.join(args.expdir, 'gifs')), 'gifs dir already exist, seems'
     init_writers(None, os.path.join(args.expdir, 'eval'))
-
-    load_path_list = glob.glob(os.path.join(args.expdir, 'model_eval_*.pt'))
-    for load_path in load_path_list:
+    eval_envs = None
+    load_paths = glob.glob(os.path.join(args.expdir, 'model_eval_*.pt'))
+    load_paths_sorted = sorted(load_paths, key=lambda s: int(s.split('_')[-1].replace('.pt', '')))
+    # for load_path in reversed(load_paths_sorted):
+    for load_path in load_paths_sorted:
         epoch = int(load_path.split('model_eval_')[-1].replace('.pt', ''))
         gifs_path_epoch = os.path.join(args.expdir, 'gifs', 'epoch{}'.format(epoch))
         if os.path.exists(gifs_path_epoch):
             print('Gif directory for epoch {} already exists, skipping the epoch...')
             continue
         start = time.time()
-        stats, gifs, num_env_steps = run_evaluation(load_path, args, device, epoch)
+        stats, gifs, num_env_steps, eval_envs = run_evaluation(load_path, args, device, epoch, eval_envs)
         print("Evaluation of epoch {} ({} steps) took {:.1f} seconds".format(
             epoch, num_env_steps, time.time() - start))
         log_eval(num_env_steps, stats)
-        if args.save_gifs:
-            if any(gifs):
-                save_gifs(args.expdir, gifs, epoch)
-            else:
-                print('All gifs are None, seems like you are running the non BCRL setup')
+        save_gifs(args.expdir, gifs, epoch)
 
 
 if __name__ == "__main__":

ppo/scripts/train.py
@@ -57,6 +57,11 @@ def _perform_actions(action_sequence, observation, policy, envs, env_render, arg
     # observation = envs.reset()
     # if env_render:
     #     env_render.reset()
+    if args.save_gifs:
+        gifs_global, gifs_local = gifs.init(args.num_processes)
+        done_before = np.array([False] * args.num_processes, dtype=np.bool)
+    else:
+        gifs_global = None
     for action in action_sequence:
         master_action_numpy = [[action] for _ in range(observation.shape[0])]
         master_action = torch.Tensor(master_action_numpy).int()
@@ -65,9 +70,19 @@ def _perform_actions(action_sequence, observation, policy, envs, env_render, arg
             if env_render is not None:
                 env_render.step(master_action[:1].numpy())
         else:
-            observation, reward, _, _ = utils.do_master_step(
-                master_action, observation, args.timescale, policy, envs, env_render)
+            observation, reward, done, _, observation_history = utils.do_master_step(
+                master_action, observation, args.timescale, policy, envs, env_render,
+                return_observations=True)
+            # TODO: change the gifs writing. so far works only when envs are done after the actions
+            if args.save_gifs:
+                # saving gifs only works for the BCRL setup
+                gifs_global, gifs_local = gifs.update(
+                    gifs_global, gifs_local, torch.tensor(master_action_numpy), done, done_before,
+                    observation_history)
+                done_before = np.logical_or(done, done_before)
         print('reward = {}'.format(reward[:, 0]))
+    if args.save_gifs:
+        gifs.save(os.path.join(args.logdir, args.timestamp), gifs_global, epoch=-1)
 
 
 def main():
@@ -127,6 +142,7 @@ def main():
         skills_check, feat_check = policy.base.resnet(test_tensor)
     if args.pudb:
         # you can call, e.g. perform_actions([0, 0, 1, 2, 3]) in the terminal
+        _perform_actions([5,0,2,1,3], obs, policy, envs, None, args)
         import pudb; pudb.set_trace()
     for epoch in range(start_epoch, num_updates):
         print('Starting epoch {}'.format(epoch))

ppo/tools/gifs.py
@@ -40,15 +40,23 @@ def save(logdir_path, gifs_list_of_dict, epoch):
     gifs_dir = os.path.join(all_gifs_dir, 'epoch{}'.format(epoch))
     if not os.path.exists(gifs_dir):
         os.mkdir(gifs_dir)
+    if gifs_list_of_dict is None:
+        return
     print('Writing the gifs to {}'.format(gifs_dir))
     for idx_gif, gif_dict in enumerate(gifs_list_of_dict):
+        if gif_dict is None:
+            print('gif_dict is None')
+            continue
         frames = gif_dict['frames']
         master_actions = gif_dict['master_actions']
         skill_actions = gif_dict['skill_actions']
         gif_name = '{:02}.gif'.format(idx_gif)
         if len(frames):
-            videos.write_video(frames, os.path.join(gifs_dir, gif_name))
-            np.savez(os.path.join(gifs_dir, '{:02}.npz'.format(idx_gif)),
-                    master_actions=master_actions,
-                    skill_actions=skill_actions)
+            try:
+                videos.write_video(frames, os.path.join(gifs_dir, gif_name))
+                np.savez(os.path.join(gifs_dir, '{:02}.npz'.format(idx_gif)),
+                        master_actions=master_actions,
+                        skill_actions=skill_actions)
+            except:
+                print('was not able to write the gif')
     print('Wrote {} gifs'.format(len(gifs_list_of_dict)))